074914af60877705f6bfed143fa2d84a3c851535
mteresamunoz <mtmm0021@red.ujaen.es>
2026-05-20 13:05:58 +0200

    certificacion hf agents

--- components/sections/certifications-section.tsx
+++ components/sections/certifications-section.tsx
@@ -23,7 +23,7 @@ const certifications = [
     key: "cert.hf",
     logo: "images/huggingface.png",
     logoAlt: "Hugging Face",
-    url: "https://cas-bridge.xethub.hf.co/xet-bridge-us/6800ea554845e4edbca48825/23529efccbc65eb43928d8c426eebe3d2fd0bcb02959b7c594d80a94569e8420?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=cas%2F20260520%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20260520T110235Z&X-Amz-Expires=3600&X-Amz-Signature=0abeec0c9ee179cfe6846bfad04e73195efc2a5a529480e5ee8575b579a9c54e&X-Amz-SignedHeaders=host&X-Xet-Cas-Uid=6784e2bfdacadead50c67c05&response-content-disposition=inline%3B+filename*%3DUTF-8%27%272026-04-29.png%3B+filename%3D%222026-04-29.png%22%3B&response-content-type=image%2Fpng&x-amz-checksum-mode=ENABLED&x-id=GetObject&Expires=1779278555&Policy=eyJTdGF0ZW1lbnQiOlt7IkNvbmRpdGlvbiI6eyJEYXRlTGVzc1RoYW4iOnsiQVdTOkVwb2NoVGltZSI6MTc3OTI3ODU1NX19LCJSZXNvdXJjZSI6Imh0dHBzOi8vY2FzLWJyaWRnZS54ZXRodWIuaGYuY28veGV0LWJyaWRnZS11cy82ODAwZWE1NTQ4NDVlNGVkYmNhNDg4MjUvMjM1MjllZmNjYmM2NWViNDM5MjhkOGM0MjZlZWJlM2QyZmQwYmNiMDI5NTliN2M1OTRkODBhOTQ1NjllODQyMCoifV19&Signature=I3hkh%7EKXoN9yBQwM965AepiAx6JhhPAbN7J6VWkeeHQzRjoPys90sLXgUfaa39KnETgG2iDU3-tddOAhFGK4DnCRVwfcIwoUmN0B-R3ykCagCvDr-IZj1VyR9Tfun3rYoil5kZG4qxRN-K-8IVGP0PQRgyoQ4llSCp8Z7kKwARY8nRkhD6La%7Eayf%7E6YthVdpMe8g7MchP3SgvEFlCSDuUNUIouQjt94LOmYM1oQ1hyGnvBvCLIXNoOWra5WEpgL7dVlJjrvzxpBoYxgIDw1FWvxahWZJHj5GEM1Ltbkby5pRDvYHUvyX1KmDJXCEUoCWNrvteD6FjiiIQjzrZUyPyQ__&Key-Pair-Id=K2L8F4GPSG1IFC",
+    url: "https://huggingface.co/datasets/agents-course/final-certificates/resolve/main/certificates/maytemuma/2026-04-29.png",
   },
   {
     key: "cert.anthropic1",
